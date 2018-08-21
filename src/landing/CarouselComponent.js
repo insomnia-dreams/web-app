c53b16b34c1ef9e68fc37d85352abbca9cc983f4
Insomnia Dreams <in.somnia.dreams@outlook.com>
2018-08-21 05:16:47 +0500

    Here we go

--- src/landing/CarouselComponent.js
+++ src/landing/CarouselComponent.js
@@ -1,26 +1,28 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
 import * as routes from '../_constants/routes'
+
+const Wrapper = styled.div`
+  margin: 4px 0;
+`
 
 const Image = styled.img`
   width: 100%;
   border-radius: 4px;
 `
 
-const Wrapper = styled.div`
-  margin: 4px 0;
-`
-
-const CarouselComponent = () => {
-  return (
-    <Wrapper>
-      <Link to={routes.SALES}>
-        <Image src="/images/carousel/carousel1.jpg" alt="banner" />
-      </Link>
-    </Wrapper>
-  )
+class CarouselComponent extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <Link to={routes.SALES}>
+          <Image src="/images/carousel/carousel1.jpg" alt="banner" />
+        </Link>
+      </Wrapper>
+    )
+  }
 }
 
 export default CarouselComponent
